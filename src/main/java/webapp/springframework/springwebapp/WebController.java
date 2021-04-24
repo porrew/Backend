4db47e7e6b1ad4cea2258de9fe726d427a974033
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-04-24 17:32:47 +0700

    spring init with working controller

---
+++ src/main/java/webapp/springframework/springwebapp/WebController.java
@@ -0,0 +1,12 @@
+package webapp.springframework.springwebapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController 
+public class WebController {
+	@RequestMapping("/")  
+	public String hello()   
+	{  
+	return "Test. again?";  
+	}
+}
